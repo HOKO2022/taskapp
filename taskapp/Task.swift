f78c91b4a276644ee363e3ecdb4c88b686cae6f5
HOKO2022 <hokosaki1205@icloud.com>
2022-02-23 00:42:16 +0900

    カテゴリ検索機能を追加した。

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by HOKO on 2022/02/21.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // カテゴリ
+    @objc dynamic var category = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
